1bbd580e2db1701a5b8804fe9bfd70b215b8afa1
jisumoon <jjisu97@naver.com>
2024-07-29 19:21:28 +0900

    4th commit

--- 24.07.28/script.js
+++ 24.07.28/script.js
@@ -1,7 +1,13 @@
-$(".myslider").slick({
-  dots: true,
-  infinite: true,
-  speed: 500,
-  fade: true,
-  cssEase: "linear",
-});
+//slick slide
+
+// $(".my_slide").slick({
+//   dots: true,
+//   infinite: true,
+//   speed: 500,
+//   fade: true,
+//   cssEase: "linear",
+//   autoplay: true,
+//   slidesToShow: 1,
+//   slidesToScroll: 1,
+//   pauseOnHover: true,
+// });
