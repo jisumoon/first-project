7db5f9e08df926885e8acb3264367e82626d166c
moonjisu <jjisu97@naver.com>
2024-08-01 00:43:05 +0900

    8th commit

--- 24.07.28/script.js
+++ 24.07.28/script.js
@@ -1,14 +1,40 @@
-//slick slider
+// 자동슬라이드
 
-// $(".my_slide").slick({
-//   dots: true,
-//   infinite: true,
-//   speed: 500,
-//   fade: true,
-//   cssEase: "linear",
-//   autoplay: true,
-// });
+const sliderWrap = document.querySelector(".inner");
+const sliderInner = document.querySelector(".my_slide");
+const slider = document.querySelectorAll(".about_content");
 
-//scroll event
+let currentIndex = 0;
+const sliderCount = slider.length;
+const sliderInterval = 7000;
+const sliderWidth = slider[0].clientWidth;
+const sliderClone = sliderInner.firstElementChild.cloneNode(true);
 
-const nav = document.querySelector("nav");
+sliderInner.appendChild(sliderClone);
+
+const sliderEffect = () => {
+  currentIndex++;
+
+  if (currentIndex > sliderCount) {
+    sliderInner.style.transition = "none";
+    sliderInner.style.transform = `translateX(0px)`;
+    currentIndex = 1;
+    setTimeout(() => {
+      sliderInner.style.transition = "all 0.6s ease";
+      sliderInner.style.transform = `translateX(-${
+        sliderWidth * currentIndex
+      }px)`;
+    }, 0);
+  } else {
+    sliderInner.style.transition = "all 0.6s ease in out";
+    sliderInner.style.transform = `translateX(-${
+      sliderWidth * currentIndex
+    }px)`;
+  }
+};
+
+setInterval(sliderEffect, sliderInterval);
+
+// 베너 색상 변화
+
+// 토글버튼
