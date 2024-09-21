9af6b2164a81ab3144742bf0dfb963a21a64b091
moonjisu <jjisu97@naver.com>
2024-09-21 23:05:15 +0900

    24.09.21

--- 24.09.18/script.js
+++ 24.09.18/script.js
@@ -12,10 +12,10 @@ slides.appendChild(firstSlideClone);
 function startSlide() {
   slideInterval = setInterval(() => {
     moveToNextSlide();
-  }, 15000);
+  }, 1500);
 }
 
-// 2. 슬라이드를 다음으로 전환하는 함수
+// 2. 슬라이드를 다음으로 전환
 function moveToNextSlide() {
   if (isTransitioning) return;
   isTransitioning = true;
@@ -41,7 +41,7 @@ function moveToNextSlide() {
   }
 }
 
-// 4. 슬라이드를 이전으로 전환하는 함수
+// 4. 슬라이드를 이전으로 전환
 function moveToPrevSlide() {
   if (isTransitioning) return;
   isTransitioning = true;
@@ -65,7 +65,7 @@ function moveToPrevSlide() {
   }
 }
 
-// 5. 슬라이드 위치를 업데이트하는 함수
+// 5. 슬라이드 위치를 업데이트
 function updateSlidePosition() {
   slides.style.transform = `translateX(-${currentIndex * 100}%)`;
 }
@@ -89,3 +89,147 @@ document.querySelector(".next").addEventListener("click", moveToNextSlide);
 
 // 10. 슬라이드 자동 시작
 startSlide();
+
+// json data
+const fetchIcecreamData = () => {
+  fetch("./data.json")
+    .then((response) => response.json())
+    .then((data) => {
+      window.icecreamData = data.data;
+      changeIcecream(1);
+    });
+};
+
+const changeIcecream = (id) => {
+  const selectedIcecream = window.icecreamData.find(
+    (icecream) => icecream.id === id
+  );
+
+  const container = document.querySelector(".sec1-container");
+
+  // html
+  container.innerHTML = `
+    <article class="sec1-Btn"  >
+          <p class="sec1-title">New, ITEM</p>
+          <div class="ice-set">
+            <div class="ice-1">
+              <img src="./img/icecream/icecream1.png" alt="icecream1" onclick="changeIcecream(1)" />
+            </div>
+            <div class="ice-2">
+              <img src="./img/icecream/icecream2.png" alt="icecream2" onclick="changeIcecream(2)" />
+            </div>
+            <div class="ice-3">
+              <img src="./img/icecream/icecream3.png" alt="icecream3"  onclick="changeIcecream(3)"/>
+            </div>
+          </div>
+        </article>
+    <figure class="main-icecream">
+      <img class="main-icecream-img" src="${selectedIcecream.image}" alt="${
+    selectedIcecream.title
+  }" />
+    </figure>
+    <article class="sec1-side" >
+      <div class="sec1-side-contain">
+        <p class="sec1-side-title">${selectedIcecream.title}</p>
+        <p class="sec1-side-desc">${selectedIcecream.description}</p>
+      </div>
+      <hr />
+      <div class="sec1-side-ing-contain">
+        <div class="ice-ing">
+          ${selectedIcecream.ingredients
+            .map(
+              (ingredient) => `
+            <div class="ice-ing-main">
+                 <div class="ice-ing-1">
+              <img src="${ingredient.image}" alt="${ingredient.name}" />
+              </div>
+              <span class="ice-ing-desc">${ingredient.name}</span>
+            </div>
+          `
+            )
+            .join("")}
+        </div>
+      </div>
+    </article>
+  `;
+};
+
+window.changeIcecream = (id) => changeIcecream(id);
+
+fetchIcecreamData();
+
+// 날짜 및 금액
+
+const animateValue = (selector, start, end, duration) => {
+  let startTimestamp = null;
+  const element = document.querySelector(selector);
+
+  const step = (timestamp) => {
+    if (!startTimestamp) startTimestamp = timestamp;
+    const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+    const currentValue = Math.floor(progress * (end - start) + start);
+
+    element.innerText = currentValue.toLocaleString() + " ₩";
+
+    if (progress < 1) {
+      requestAnimationFrame(step);
+    }
+  };
+
+  requestAnimationFrame(step);
+};
+
+animateValue(".donation-amount", 0, 2221986, 100000);
+
+const updateDonationDate = () => {
+  const dateElement = document.querySelector("#donation-date");
+  const today = new Date();
+
+  const year = today.getFullYear();
+  const month = String(today.getMonth() + 1).padStart(2, "0");
+  const day = String(today.getDate()).padStart(2, "0");
+
+  const formattedDate = `${year}년 ${month}월 ${day}일 기준`;
+  dateElement.innerText = formattedDate;
+};
+
+updateDonationDate();
+
+//이미지
+
+const images = [
+  "./img/intro/intro1.png",
+  "./img/intro/intro2.png",
+  "./img/intro/intro3.png",
+];
+
+const backgroundColors = ["#ffb38e", "#FFF3C7", "#FFB0B0"];
+
+const changeImageAndBackground = () => {
+  const imgElement = document.querySelector(".sec3-circle img");
+  const backgroundElement = document.querySelector(".sec3-circle");
+  currentIndex = (currentIndex + 1) % images.length;
+  imgElement.src = images[currentIndex];
+  backgroundElement.style.backgroundColor = backgroundColors[currentIndex];
+};
+
+setInterval(changeImageAndBackground, 2000);
+
+// mini 공
+const miniBackgroundColors = [
+  "#EF9595",
+  "#EFB495",
+  "#EFD595",
+  "#EBEF95",
+  "FFE5E5",
+];
+
+let currentMiniIndex = 0;
+
+const changeMiniBackground = () => {
+  const miniCircle = document.querySelector(".sec3-mini-circle");
+  miniCircle.style.backgroundColor = miniBackgroundColors[currentMiniIndex];
+  currentMiniIndex = (currentMiniIndex + 1) % miniBackgroundColors.length;
+};
+
+setInterval(changeMiniBackground, 2500);
